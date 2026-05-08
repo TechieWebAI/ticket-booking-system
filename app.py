b86f0037d37d700f31faa4a836ba8b47870ba1f3
TechieWebAI <246836017+TechieWebAI@users.noreply.github.com>
2026-05-08 22:43:16 +0530

    Fixed database setup for deployment

--- app.py
+++ app.py
@@ -109,7 +109,9 @@ def release_expired_locks():
             seat.lock_time = None
     db.session.commit()
 
-if __name__ == "__main__":
+with app.app_context():
     create_tables()
+
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
